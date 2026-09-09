45eed235ac3e4b30a21afcf1118bad5d02587ea0
Evan Abbott <107959883+EvanA4@users.noreply.github.com>
2026-09-09 13:31:54 -0400

    project init

---
+++ src/main/java/net/eabbott/meggo/MeggoDataGenerator.java
@@ -0,0 +1,11 @@
+package net.eabbott.meggo;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MeggoDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
